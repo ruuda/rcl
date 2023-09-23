ac88c5b0d280a3425c8b6549c9a05c940deca6b6
Ruud van Asseldonk <dev@veniogames.com>
2023-09-23 12:42:55 +0200

    Make the json formatter stateful
    This is the first part to tracking where in the document we are,
    so we can report a useful error location. The error type still needs to
    be updated to support this though.

--- src/json.rs
+++ src/json.rs
@@ -7,6 +7,8 @@
 
 //! Conversion from and to json.
 
+use std::rc::Rc;
+
 use crate::error::{Result, ValueError};
 use crate::pprint::{concat, group, indent, Doc};
 use crate::runtime::Value;
@@ -15,96 +17,119 @@ use crate::string::escape_json;
 
 /// Render a value as json.
 pub fn format_json(caller: Span, v: &Value) -> Result<Doc> {
-    // TODO: Instead of the caller span, we should have a dedicated error type
-    // for reporting runtime errors.
-    let result: Doc = match v {
-        Value::Null => "null".into(),
-        Value::Bool(true) => "true".into(),
-        Value::Bool(false) => "false".into(),
-        Value::Int(i) => i.to_string().into(),
-        Value::String(s) => {
-            let mut into = String::with_capacity(s.len());
-            escape_json(s, &mut into);
-            concat! { "\"" into "\"" }
-        }
-        Value::List(vs) => {
-            let mut elements = Vec::new();
-            let mut is_first = true;
-            for v in vs {
-                if !is_first {
-                    elements.push(",".into());
-                    elements.push(Doc::Sep);
-                }
-                elements.push(format_json(caller, v)?);
-                is_first = false;
-            }
-            group! {
-                "["
-                Doc::SoftBreak
-                indent! { Doc::Concat(elements) }
-                Doc::SoftBreak
-                "]"
-            }
-        }
-        Value::Set(vs) => {
-            // TODO: Deduplicate between list/set.
-            let mut elements = Vec::new();
-            let mut is_first = true;
-            for v in vs {
-                if !is_first {
-                    elements.push(",".into());
-                    elements.push(Doc::Sep);
-                }
-                elements.push(format_json(caller, v)?);
-                is_first = false;
-            }
-            group! {
-                "["
-                Doc::SoftBreak
-                indent! { Doc::Concat(elements) }
-                Doc::SoftBreak
-                "]"
-            }
-        }
-        Value::Map(vs) => {
-            let mut elements = Vec::new();
-            let mut is_first = true;
-            for (k, v) in vs {
-                if !is_first {
-                    elements.push(",".into());
-                    elements.push(Doc::Sep);
-                }
-                match k.as_ref() {
-                    Value::String(..) => elements.push(format_json(caller, k)?),
-                    _ => {
-                        let err = ValueError {
-                            span: caller,
-                            // TODO: Include information about the encountered type.
-                            message: "To export as json, keys must be strings.",
-                        };
-                        return Err(err.into());
-                    }
-                };
-                elements.push(": ".into());
-                elements.push(format_json(caller, v)?);
-                is_first = false;
-            }
-            group! {
-                "{"
-                Doc::SoftBreak
-                indent! { Doc::Concat(elements) }
-                Doc::SoftBreak
-                "}"
-            }
-        }
-        Value::Builtin(..) => {
-            let err = ValueError {
-                span: caller,
-                message: "Functions cannot be exported as json.",
-            };
-            return Err(err.into());
-        }
-    };
+    let mut formatter = Formatter::new(caller);
+    formatter.value(v)
+}
 
-    Ok(result)
+/// Element of a path through a value.
+enum PathElement {
+    Key(Rc<str>),
+    Index(usize),
+}
+
+/// Helper for formatting values as json.
+///
+/// The formatter tracks the path in the value that we are formatting from, such
+/// that we can report the location of an error, in case an error occurs.
+struct Formatter {
+    /// The source location where json formatting was triggered from.
+    caller: Span,
+
+    /// Where we currently are in the value to be formatted.
+    path: Vec<PathElement>,
+}
+
+impl Formatter {
+    pub fn new(caller: Span) -> Formatter {
+        Formatter {
+            caller,
+            path: Vec::new(),
+        }
+    }
+
+    /// Report an error at the current value path.
+    fn error<T>(&self, message: &'static str) -> Result<T> {
+        // TODO: Enable the path in ValueError.
+        let err = ValueError {
+            span: self.caller,
+            message,
+        };
+        return Err(err.into());
+    }
+
+    fn list<'s, 'a>(&'s mut self, vs: impl Iterator<Item = &'a Rc<Value>>) -> Result<Doc<'a>> {
+        let mut elements = Vec::new();
+        let mut is_first = true;
+        for (i, v) in vs.enumerate() {
+            if !is_first {
+                elements.push(",".into());
+                elements.push(Doc::Sep);
+            }
+            self.path.push(PathElement::Index(i));
+            elements.push(self.value(v)?);
+            self.path.pop().expect("Push and pop are balanced.");
+            is_first = false;
+        }
+        let result = group! {
+            "["
+            Doc::SoftBreak
+            indent! { Doc::Concat(elements) }
+            Doc::SoftBreak
+            "]"
+        };
+        Ok(result)
+    }
+
+    fn object<'s, 'a>(
+        &'s mut self,
+        vs: impl Iterator<Item = (&'a Rc<Value>, &'a Rc<Value>)>,
+    ) -> Result<Doc<'a>> {
+        let mut elements = Vec::new();
+        let mut is_first = true;
+        for (k, v) in vs {
+            if !is_first {
+                elements.push(",".into());
+                elements.push(Doc::Sep);
+            }
+            match k.as_ref() {
+                Value::String(k_str) => {
+                    self.path.push(PathElement::Key(k_str.clone()));
+                    elements.push(self.value(k)?)
+                }
+                // TODO: Include information about the encountered type.
+                _ => return self.error("To export as json, keys must be strings."),
+            };
+            elements.push(": ".into());
+            elements.push(self.value(v)?);
+            self.path.pop().expect("Push and pop are balanced.");
+            is_first = false;
+        }
+        let result = group! {
+            "{"
+            Doc::SoftBreak
+            indent! { Doc::Concat(elements) }
+            Doc::SoftBreak
+            "}"
+        };
+        Ok(result)
+    }
+
+    fn value<'s, 'a>(&'s mut self, v: &'a Value) -> Result<Doc<'a>> {
+        let result: Doc = match v {
+            Value::Null => "null".into(),
+            Value::Bool(true) => "true".into(),
+            Value::Bool(false) => "false".into(),
+            Value::Int(i) => i.to_string().into(),
+            Value::String(s) => {
+                let mut into = String::with_capacity(s.len());
+                escape_json(s, &mut into);
+                concat! { "\"" into "\"" }
+            }
+            Value::List(vs) => self.list(vs.iter())?,
+            Value::Set(vs) => self.list(vs.iter())?,
+            Value::Map(vs) => self.object(vs.iter())?,
+            Value::Builtin(..) => self.error("Functions cannot be exported as json.")?,
+        };
+        Ok(result)
+    }
 }
