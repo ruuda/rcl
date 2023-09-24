720898f74f22a5c5fa6ed16b2ffdeef9fcb96b9a
Ruud van Asseldonk <dev@veniogames.com>
2023-09-24 20:48:01 +0200

    Implement format strings in the Pygments formatter

--- etc/pygments/rcl.py
+++ etc/pygments/rcl.py
@@ -14,6 +14,66 @@
 from pygments.lexer import RegexLexer, words
 from pygments import token
 
+# Due to the handling of string interpolation, we have a few states that are
+# almost identical to the root state, but differ in the handling of `}`.
+# Therefore we extract the common tokens here.
+_root_base = [
+    (r"#!.*?$", token.Comment.Hashbang),
+    (r"//.*?$", token.Comment),
+    (r'f"""', token.String, "format_triple"),
+    (r'"""', token.String, "string_triple"),
+    (r'f"', token.String, "format_double"),
+    (r'"', token.String, "string_double"),
+    # Note, the `}` we handle in each of the specializations of this state.
+    (r"0b[01_]+", token.Number.Bin),
+    (r"0x[0-9a-fA-F_]+", token.Number.Hex),
+    (r"[0-9_]+(\.[0-9_]+)?([eE][+-]?[0-9_]+)?", token.Number),
+    # In the Rust lexer, there is one state for identifiers, and in
+    # there we recognize the keywords. We don't recognize builtins at
+    # all in the Rust lexer. Here, we do split all those out by token
+    # type.
+    (
+        words(
+            (
+                "and",
+                "else",
+                "false",
+                "for",
+                "if",
+                "in",
+                "let",
+                "not",
+                "null",
+                "or",
+                "then",
+                "true",
+            ),
+            suffix=r"\b",
+        ),
+        token.Keyword,
+    ),
+    (
+        words(
+            (
+                "contains",
+                "get",
+                "len",
+            ),
+            suffix=r"\b",
+        ),
+        token.Name.Builtin,
+    ),
+    (r"[_a-z][_a-z0-9-]*", token.Name),
+    (r"\s+", token.Whitespace),
+    # In the Rust lexer the punctuation is split out, and then further
+    # into digraphs and monographs. Here we instead split them out by
+    # token type.
+    (r"<=|>=|==|!=|<|>|\+|-|\*|/|\|", token.Operator),
+    (r"[)(\]\[=,.:;]", token.Token),
+    (r"{", token.Token, "in_brace"),
+    (r"#", token.Error),
+]
+
 
 class RclLexer(RegexLexer):
     """
@@ -27,74 +87,44 @@ class RclLexer(RegexLexer):
     tokens = {
         # The root state here corresponds to `Lexer::next` in Rust.
         "root": [
-            (r"#!.*?$", token.Comment.Hashbang),
-            (r"//.*?$", token.Comment),
-            (r'f"""', token.String, "format_triple_open"),
-            (r'"""', token.String, "string_triple"),
-            (r'f"', token.String, "format_double_open"),
-            (r'"', token.String, "string_double"),
-            # TODO: Handle the }
-            (r"0b[01_]+", token.Number.Bin),
-            (r"0x[0-9a-fA-F_]+", token.Number.Hex),
-            (r"[0-9_]+(\.[0-9_]+)?([eE][+-]?[0-9_]+)?", token.Number),
-            # In the Rust lexer, there is one state for identifiers, and in
-            # there we recognize the keywords. We don't recognize builtins at
-            # all in the Rust lexer. Here, we do split all those out by token
-            # type.
-            (
-                words(
-                    (
-                        "and",
-                        "else",
-                        "false",
-                        "for",
-                        "if",
-                        "in",
-                        "let",
-                        "not",
-                        "null",
-                        "or",
-                        "then",
-                        "true",
-                    ),
-                    suffix=r"\b",
-                ),
-                token.Keyword,
-            ),
-            (
-                words(
-                    (
-                        "contains",
-                        "get",
-                        "len",
-                    ),
-                    suffix=r"\b",
-                ),
-                token.Name.Builtin,
-            ),
-            (r"[_a-z][_a-z0-9-]*", token.Name),
-            (r"\s+", token.Whitespace),
-            # In the Rust lexer the punctuation is split out, and then further
-            # into digraphs and monographs. Here we instead split them out by
-            # token type.
-            (r"<=|>=|==|!=|<|>|\+|-|\*|/|\|", token.Operator),
-            (r"[)(}{\]\[=,.:;]", token.Token),
-            (r"#", token.Error),
+            (r"}", token.Error),
+            *_root_base,
         ],
-        "format_double_open": [],
-        "format_triple_open": [],
-        "string_double": [
-            (r'[^\\"]+', token.String),
-            (r"\\", token.String.Escape, "escape"),
+        "in_brace": [
+            (r"}", token.Token, "#pop"),
+            *_root_base,
+        ],
+        "in_interpolation": [
+            (r"}", token.String.Interpol, "#pop"),
+            *_root_base,
+        ],
+        "format_double": [
+            (r'[^\\"{]+', token.String),
             (r'"', token.String, "#pop"),
+            (r"{", token.String.Interpol, "in_interpolation"),
+            (r"\\", token.String.Escape, "escape"),
         ],
-        "string_triple": [
-            (r'[^\\"]+', token.String),
+        "format_triple": [
+            (r'[^\\"{]+', token.String),
+            (r'"""', token.String, "#pop"),
             # Only """ ends the string, but " cannot occur in the above state,
             # so list those two explicitly.
             (r'""|"', token.String),
             (r"\\", token.String.Escape, "escape"),
+            (r"{", token.String.Interpol, "in_interpolation"),
+        ],
+        "string_double": [
+            (r'[^\\"]+', token.String),
+            (r'"', token.String, "#pop"),
+            (r"\\", token.String.Escape, "escape"),
+        ],
+        "string_triple": [
+            (r'[^\\"]+', token.String),
             (r'"""', token.String, "#pop"),
+            # Only """ ends the string, but " cannot occur in the above state,
+            # so list those two explicitly.
+            (r'""|"', token.String),
+            (r"\\", token.String.Escape, "escape"),
         ],
         "escape": [
             (r'["\\/bfnrt{]', token.String.Escape, "#pop"),
