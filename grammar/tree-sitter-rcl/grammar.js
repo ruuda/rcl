aa7b9b26201ea295f4237b6f1f3e18f11a3e90bc
Ruud van Asseldonk <dev@veniogames.com>
2024-03-06 19:19:37 +0100

    TS: Work towards parsing the tags example
    It parses some things, but it doesn't pick up my let ... Maybe because
    it fails to parse the remaining expressions and it doesn't recover from
    that?

--- grammar/tree-sitter-rcl/grammar.js
+++ grammar/tree-sitter-rcl/grammar.js
@@ -6,13 +6,25 @@
 // A copy of the License has been included in the root of the repository.
 
 module.exports = grammar({
-  name: 'rcl',
+  name: "rcl",
 
   rules: {
     source_file: $ => $._prefixed_expr,
 
-    // Hmm, do I really need to define an external scanner already? ...
-    // Ah wait no, only for some tokens. I need to look into this ...
-    _prefixed_expr: $ => "TODO",
+    // A blank is whitespace with two or more newlines.
+    // Space allows arbitrary newlines, but they are not preserved in the CST.
+    blank: $ => /[ \t\r\f]*\n[ \t\r\f]*\n[ \t\r\n\f]*/,
+    _space: $ => /[ \t\r\n\f]+/,
+    comment: $ => seq("//", /[^\n]*\n/),
+    _prefix: $ => choice($.blank, $.comment, $._space),
+
+    ident: $ => /[_A-Za-z][-_A-Za-z0-9]*/,
+
+    _prefixed_expr: $ => seq(repeat($._prefix), $._expr),
+    _expr: $ => choice($.expr_stmt),
+    expr_stmt: $ => seq($._stmt, $._space, ";", $._prefixed_expr),
+
+    _stmt: $ => choice($.stmt_let),
+    stmt_let: $ => seq("let", $._space, $.ident, $._space, "=", $._prefixed_expr),
   }
 });
