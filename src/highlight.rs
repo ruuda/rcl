2ae42619222d5e2c5df7f808712294e5ec271b4d
Ruud van Asseldonk <dev@veniogames.com>
2024-03-26 00:13:03 +0100

    Rewrite highlight command using MarkupString
    This will enable it to be used from the webassembly module, and also
    gets rid of the duplication for the ansi escape codes. I tried hard to
    get the Tree-sitter highlight crate to work with webassembly, but it's
    just too much of a mess and debugging linker errors is one of those
    things that always takes an entire night, where you feel on the verge
    of a breakthrough, but in the end it fails and you end up feeling
    miserable. So no Tree-sitter wasm, maybe I can just use this
    highlighter, it is less robust and less fancy, but it's better than
    nothing.
    
    This temporarily removes highlighting built-ins, maybe I can restore it
    again later. Or maybe I can just highlight any function call by looking
    ahead for a paren.

--- src/highlight.rs
+++ src/highlight.rs
@@ -7,39 +7,45 @@
 
 //! A syntax highlighter that acts on the token stream.
 
-use std::io::{Result, Write};
-
 use crate::lexer::{Lexeme, Token};
+use crate::markup::{Markup, MarkupString};
 
-fn get_color(token: &Token, token_bytes: &[u8]) -> &'static str {
-    let red = "\x1b[31m";
-    let green = "\x1b[32m";
-    let blue = "\x1b[34m";
-    let cyan = "\x1b[36m";
-    let white = "\x1b[37m";
-    let magenta = "\x1b[35m";
-    let reset = "\x1b[0m";
+// TODO: These are now unused. Bring back highlighting of builtins.
+#[allow(dead_code)]
+const BUILTINS: &[&str] = &[
+    "chars",
+    "contains",
+    "ends_with",
+    "enumerate",
+    "except",
+    "fold",
+    "get",
+    "group_by",
+    "join",
+    "key_by",
+    "keys",
+    "len",
+    "parse_int",
+    "remove_prefix",
+    "remove_suffix",
+    "replace",
+    "reverse",
+    "split",
+    "split_lines",
+    "starts_with",
+    "std",
+    "to_lowercase",
+    "to_uppercase",
+    "values",
+];
 
+fn get_markup(token: &Token) -> Markup {
     match token {
-        Token::Space | Token::Blank => reset,
-        Token::LineComment => white,
-        Token::NumBinary | Token::NumHexadecimal | Token::NumDecimal => cyan,
-        Token::Ident => match token_bytes {
-            // Give the builtins a different color.
-            // TODO: Only when preceded by a dot, when they are methods.
-            b"chars" | b"contains" | b"ends_with" | b"enumerate" | b"except" | b"fold" | b"get"
-            | b"group_by" | b"join" | b"key_by" | b"keys" | b"len" | b"parse_int"
-            | b"remove_prefix" | b"remove_suffix" | b"replace" | b"reverse" | b"split"
-            | b"split_lines" | b"starts_with" | b"std" | b"to_lowercase" | b"to_uppercase"
-            | b"values" => red,
-            _ => blue,
-        },
-
-        Token::QuoteOpen(..) | Token::QuoteClose | Token::StringInner => cyan,
-
-        Token::HoleOpen | Token::HoleClose => red,
-
-        Token::Escape(..) => magenta,
+        Token::LineComment => Markup::Comment,
+        Token::NumBinary | Token::NumHexadecimal | Token::NumDecimal => Markup::Number,
+        Token::QuoteOpen(..) | Token::QuoteClose | Token::StringInner => Markup::String,
+        Token::HoleOpen | Token::HoleClose | Token::Escape(..) => Markup::Escape,
+        Token::Ident => Markup::Field,
 
         Token::KwAnd
         | Token::KwAssert
@@ -53,36 +59,29 @@ fn get_color(token: &Token, token_bytes: &[u8]) -> &'static str {
         | Token::KwNull
         | Token::KwOr
         | Token::KwTrace
-        | Token::KwTrue => green,
+        | Token::KwTrue => Markup::Keyword,
 
-        _ => reset,
+        _ => Markup::None,
     }
 }
 
-pub fn highlight(out: &mut dyn Write, tokens: &[Lexeme], input: &str) -> Result<()> {
-    let input = input.as_bytes();
+pub fn highlight<'a>(tokens: &[Lexeme], input: &'a str) -> MarkupString<'a> {
+    let mut out = MarkupString::new();
     let mut end = 0;
-    let mut color = "";
 
     for (token, span) in tokens {
-        let token_bytes = &input[span.start()..span.end()];
-        let new_color = get_color(token, token_bytes);
-        if new_color != color {
-            out.write_all(new_color.as_bytes())?;
-            color = new_color;
-        }
-
         // Insignificant space is not represented explicitly as a token, we can
         // infer it from a gap in the byte range.
         if span.start() > end {
-            out.write_all(&input[end..span.start()])?;
+            out.push(&input[end..span.start()], Markup::None);
         }
 
-        out.write_all(token_bytes)?;
+        let markup = get_markup(token);
+        let string = &input[span.start()..span.end()];
+        out.push(string, markup);
+
         end = span.end();
     }
 
-    out.write_all(&input[end..])?;
-
-    Ok(())
+    out
 }
