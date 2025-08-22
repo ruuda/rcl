0658eb0a709780afcc3ebe3c5d4b4696a8df04ca
Ruud van Asseldonk <dev@veniogames.com>
2025-08-22 19:47:27 +0200

    Add regression test for 1e0 number format
    This makes the tests fail. The fix will be in a follow-up commit, to
    ensure that the test would have caught the bug.

--- src/decimal.rs
+++ src/decimal.rs
@@ -658,6 +658,9 @@ mod test {
         // We should be able to reach the maximum exponents.
         assert_parse_decimal("1e32767", 1, 0, i16::MAX);
         assert_parse_decimal("1e-32768", 1, 0, i16::MIN);
+
+        // This is a regression test, e0 we parse as exponent 0.
+        assert_parse_decimal("1e0", 1, 0, 0);
     }
 
     #[test]
